76a5250e1ac41568bc294933041a90ecae6a2103
Anirudh Jwala <anirudhsai2@gmail.com>
2020-12-04 23:07:06 +0530

    Configuring with postgreSQL

--- tubers/settings.py
+++ tubers/settings.py
@@ -63,8 +63,11 @@ WSGI_APPLICATION = 'tubers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lcotubers',
+        'USER': 'postgres',
+        'PASSWORD': 'anirudh',
+        'HOST': 'localhost'
     }
 }
 
