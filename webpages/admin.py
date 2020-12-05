3a458f4ad1ec507fb18011fbfb7eef90b3553b21
Anirudh Jwala <anirudhsai2@gmail.com>
2020-12-05 16:45:29 +0530

    Added team members onto home page

--- webpages/admin.py
+++ webpages/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 
 from .models import Slider, Team
 
+from django.utils.html import format_html
+
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'role', 'created_date')
+
+    def team_member_photo(self, object):
+        return format_html('<img src="{}" width="40px" />'.format(object.photo.url))
+
+    list_display = ('id', 'team_member_photo',
+                    'first_name', 'role', 'created_date')
     list_display_links = ('first_name',)
     search_fields = ('first_name', 'last_name', 'role')
     list_filter = ('role',)
