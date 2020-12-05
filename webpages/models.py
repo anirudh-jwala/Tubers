82bb44985a2997c4e14c8b2b897dc57524067e15
Anirudh Jwala <anirudhsai2@gmail.com>
2020-12-05 15:51:52 +0530

    Registering sliders on Admin panel and uploading new data

--- webpages/models.py
+++ webpages/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Slider(models.Model):
     headline = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
@@ -9,3 +9,6 @@ class Slider(models.Model):
     photo = models.ImageField(upload_to='media/slider/%Y/')
 
     created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.headline
