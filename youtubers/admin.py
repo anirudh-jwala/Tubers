a92c456e26876eaf0168ba92dbb112f0c824f9b9
Anirudh Jwala <anirudhsai2@gmail.com>
2020-12-05 23:27:17 +0530

    Customizing admin panle youtuber listing

--- youtubers/admin.py
+++ youtubers/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 from .models import Youtuber
 
-admin.site.register(Youtuber)
+from django.utils.html import format_html
+
+
+class YtAdmin(admin.ModelAdmin):
+
+    def youtuber_photo(self, object):
+        return format_html('<img src="{}" width="40px" />'.format(object.photo.url))
+
+    list_display = ('id', 'name', 'youtuber_photo','subs_count', 'is_featured')
+    search_fields = ('name', 'camera_type', 'category')
+    list_filter = ('city', 'camera_type', 'category')
+    list_display_links = ('id', 'name')
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Youtuber, YtAdmin)
