7fc7b208cc03f1b089cfa8830d095d36d837bcb9
Anirudh Jwala <anirudhsai2@gmail.com>
2020-12-05 16:07:56 +0530

    Functional slider button with default redirect

--- webpages/models.py
+++ webpages/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Slider(models.Model):
     headline = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
+
     button_text = models.CharField(max_length=255)
+    button_url = models.CharField(
+        max_length=255, default='https://anirudhjwala.me')
 
     photo = models.ImageField(upload_to='media/slider/%Y/')
 
