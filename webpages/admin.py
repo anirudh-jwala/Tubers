a8bd4c737134ca95b50db8a47bc674cccace83ef
Anirudh Jwala <anirudhsai2@gmail.com>
2020-12-05 16:30:33 +0530

    Adding team members, customizing admin panel

--- webpages/admin.py
+++ webpages/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Slider
-# Register your models here.
+from .models import Slider, Team
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'role', 'created_date')
+    list_display_links = ('first_name',)
+    search_fields = ('first_name', 'last_name', 'role')
+    list_filter = ('role',)
+
 
 admin.site.register(Slider)
+admin.site.register(Team, TeamAdmin)
--- webpages/models.py
+++ webpages/models.py
@@ -15,3 +15,20 @@ class Slider(models.Model):
 
     def __str__(self):
         return self.headline
+
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    role = models.CharField(max_length=255)
+
+    facebook_link = models.CharField(max_length=255)
+    instagram_link = models.CharField(max_length=255)
+
+    photo = models.ImageField(upload_to='media/team/%Y/%m/%d/')
+
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
