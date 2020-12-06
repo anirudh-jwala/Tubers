26c5d6e0832928ca0dbe676259bf987409ed6d9d
Anirudh Jwala <anirudhsai2@gmail.com>
2020-12-06 10:35:17 +0530

    Added more youtubers, updated feature section on home page that updates dynamically

--- webpages/views.py
+++ webpages/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 
 from .models import Slider, Team
 
+from youtubers.models import Youtuber
+
 
 def home(request):
     sliders = Slider.objects.all()
     teams = Team.objects.all()
 
+    featured_youtubers = Youtuber.objects.order_by(
+        '-created_date').filter(is_featured=True)
+
     data = {
         'sliders': sliders,
-        'teams': teams
+        'teams': teams,
+        'featured_youtubers': featured_youtubers,
     }
     return render(request, 'webpages/home.html', data)
 
