d49b96e87dda911929ff0cd701379cacc8b97c2d
Anirudh Jwala <anirudhsai2@gmail.com>
2020-12-07 14:06:21 +0530

    User registration views

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth import logout
 
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
+
 
 def login(request):
     return render(request, 'accounts/login.html')
 
 
 def register(request):
+
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        user = User.objects.create_user(
+            firstname=firstname, lastname=lastname, username=username, email=email, password=password)
+        user.save()
+
+        messages.success(request, 'Account created successfully')
+        return redirect('login')
+
     return render(request, 'accounts/register.html')
 
 
