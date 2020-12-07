ab7c007adafdd05a0aa01a2ebbd1bac4aabadd0e
Anirudh Jwala <anirudhsai2@gmail.com>
2020-12-07 14:20:01 +0530

    Registering new user with safety checks and adding details to DB

--- accounts/views.py
+++ accounts/views.py
@@ -20,12 +20,25 @@ def register(request):
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
 
-        user = User.objects.create_user(
-            firstname=firstname, lastname=lastname, username=username, email=email, password=password)
-        user.save()
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already in use')
+                return redirect('register')
 
-        messages.success(request, 'Account created successfully')
-        return redirect('login')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(
+                        first_name=firstname, last_name=lastname, username=username, email=email, password=password)
+                    user.save()
+
+                    messages.success(request, 'Account created successfully')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')
 
     return render(request, 'accounts/register.html')
 
